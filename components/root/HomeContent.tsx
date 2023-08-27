b3cf66b0604a6613cc2271b8f7830fd81474f416
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-08-27 18:05:32 +0100

    feat: Fix responsiveness

--- components/root/HomeContent.tsx
+++ components/root/HomeContent.tsx
@@ -17,7 +17,6 @@ const HomeContent = () => {
           variants={HeaderAnimationVariant(index, delayY)}
           initial="hidden"
           whileInView="show"
-          
         >
           {letter === ' ' ? '\u00A0' : letter}
         </motion.span>
@@ -25,25 +24,31 @@ const HomeContent = () => {
   }
   
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-    <div className="text-center mb-8">
-      <div className="text-4xl font-bold mb-2 flex">
+    <div className="flex flex-col items-center md:justify-center justify-normal min-h-screen px-4 mt-14">
+      <div className="text-center mb-8">
+        <div className="text-2xl md:text-4xl font-bold mb-4">
             {animateText(textContent, 0.01)}
         </div>
-      <p className="text-lg text-gray-600 dark:text-gray-400">
+        <p 
+         className="
+           text-base md:text-lg text-gray-600
+            md:flex-nowrap md:mx-0 md:w-full
+          dark:text-gray-400 flex flex-wrap
+            w-4/5 mx-auto 
+          ">
             {animateText(textContent2, 0.05)}
-      </p>
-    </div>
-    <div className="border border-dark-primary p-3">
-      <p className="text-lg text-gray-800 dark:text-gray-100">
-        Effortlessly manage job applications, track progress, and gain insights into your career path.
-        Let Pletonia guide you to your dream job! 🌠
-      </p>
-    </div>
-    <button className="mt-8 bg-dark-primary text-white px-6 py-3 rounded-md shadow-lg hover:bg-primary hover:text-dark-primary border hover:border-dark-primary border-dark-primary transition-colors">
-      Get Started
-    </button>
-    <footer className="mt-16 flex items-center justify-center text-dark-primary dark:text-secondary space-x-6">
+        </p>
+      </div>
+      <div className="border border-dark-primary p-3">
+        <p className="text-base md:text-lg text-gray-800 dark:text-gray-100">
+          Effortlessly manage job applications, track progress, and gain insights into your career path.
+          Let Pletonia guide you to your dream job! 🌠
+        </p>
+      </div>
+      <button className="mt-14 md:mt-8 bg-dark-primary text-white px-6 py-3 rounded-md shadow-lg hover:bg-primary hover:text-dark-primary border hover:border-dark-primary border-dark-primary transition-colors">
+        Get Started
+      </button>
+      <footer className="mt-12 md:mt-16 flex items-center justify-center text-dark-primary dark:text-secondary space-x-4 md:space-x-6">
         <Link href="https://github.com/emmaGH1" target="_blank">
           <BsGithub className="text-xl hover:text-dark-accent transition-colors" />
         </Link>
@@ -53,11 +58,11 @@ const HomeContent = () => {
         <Link href="https://twitter.com/emmadotdev" target="_blank">
            <BsTwitter className="text-xl hover:text-dark-accent transition-colors" />
         </Link>
-    </footer>
-
-    <p className="mt-4 text-gray-500 dark:text-gray-400">Copyright © 2023 Pletonia</p>
-  </div>
-
+      </footer>
+      <p className="mt-4 text-sm md:text-base text-gray-500 dark:text-gray-400">
+        Copyright © 2023 Pletonia
+      </p>
+    </div>
   );
 };
 
