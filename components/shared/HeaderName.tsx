2b76beded3b15ac625eaac7850cb07f9d985c2fd
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-08-29 22:02:21 +0100

    feat: Add className prop to component

--- components/shared/HeaderName.tsx
+++ components/shared/HeaderName.tsx
@@ -2,13 +2,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const HeaderName = () => {
+const HeaderName = ({ className }: { className?: string }) => {
   return (
-    <Link className="flex items-center" href='/' >
+    <Link className={`flex items-center ${className}`} href='/' >
     <div className="dark:shadow-inner">
       <Image src="/logo.svg" alt='logo' width={30} height={30} className="hidden md:block dark:invert"/>
     </div>
-    <h1 className="text-3xl ml-2 font-bold">Pletonia</h1>
+    <h1 className="text-3xl ml-2 font-bold font-heading">Pletonia</h1>
   </Link>
   );
 };
