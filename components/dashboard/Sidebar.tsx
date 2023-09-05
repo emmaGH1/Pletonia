9c9b0b0283273d148a9574015e63f9becfca5bac
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-09-05 01:30:41 +0100

    feat: Add style for active and hover events

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -1,11 +1,16 @@
+'use client'
+
 import { navbarLinks } from "@/constants";
 import HeaderName from "../shared/HeaderName";
 import { createElement } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ToggleButton } from "../shared/ToggleButton";
 
 
 const Sidebar = () => {
+  const pathname = usePathname()
+
   return (
     <nav className="sidebar dark:bg-dark-secondary bg-secondary">
       <HeaderName className=""/>
@@ -13,16 +18,16 @@ const Sidebar = () => {
       <div className="flex flex-col w-4/5 justify-center items-center mt-10">
         {navbarLinks.map((each) => {
                 return (
-                    <Link href={each.route} className="flex my-5 items-center" key={each.label}>
+                    <Link href={each.route} className={`flex my-5 items-center hover:text-dark-accent dark:hover:text-dark-accent transition-colors duration-200 ${each.route === pathname ? '' : ' text-dark-primary/60 dark:text-primary/60'}`} key={each.label} 
+                    >
                     <span className="mr-2">{each.label}</span>
                     <div>{createElement(each.icon)}</div>
                     </Link>
                 );
-                })}
-        <div className="flex w-full items-center justify-center mt-10 bg-dark-accent rounded-xl">
-            <span className="flex whitespace-nowrap">Theme</span>
-           <ToggleButton /> 
-        </div>  
+        })}
+
+        <ToggleButton className="flex w-full items-center justify-center mt-10 bg-dark-accent rounded-xl text-white"/> 
+      
       </div>
     </nav>
   );
